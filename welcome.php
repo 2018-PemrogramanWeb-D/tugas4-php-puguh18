a6af185ee3096bb24980dd5038c8f0f90a940598
puguh1818 <puguh.sd@gmail.com>
2018-10-12 17:28:22 +0700

    Tugas 4
    Form and all

---
+++ welcome.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<body>
+	
+<h1>HAHAHA</h1>
+
+Email kamu : "<?php
+echo  $_POST["Email"];?>"
+<br>
+Password kamu : "<?php echo $_POST["Password"];?>"
+<!--Kamu adalah : <?php echo $POST[""];?>-->
+</body>
+</html>
